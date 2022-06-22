e7997da89a4da98d8b6df00e906b35ba6528776a
Numan Anees <numananees44@gmail.com>
2022-06-22 19:28:04 +0500

    scope set

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const { User } = require("../models");
 
 exports.getAll = async (req, res) => {
   try {
-    const allUsers = await User.findAll();
+    const allUsers = await User.scope("name").findAll();
     res.status(200).json({ data: allUsers });
   } catch (err) {
     console.log(err);
